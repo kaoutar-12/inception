b2cfed6380711b2797e9c1da2feb9eac82f7857d
kaoutar-12 <kmouradi@student.1337.ma>
2024-03-11 09:23:46 +0000

    done

--- srcs/requirements/mariadb/conf/script.sh
+++ srcs/requirements/mariadb/conf/script.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-service mysql start;
-
 mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE";
 mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'";
 mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'";
 mysql -e "FLUSH PRIVILEGES"; 
 mysql -e "ALTER USER '$MYSQL_ROOT'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'";
-
-service mysql stop;
-
-touch /var/lib/mysql/.setup
 
 mysqld_safe
